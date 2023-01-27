599d010949e626886f2f5a0d9ad5fd95ea821967
Joey Santia <joey.santia@gmail.com>
2023-01-27 12:53:49 -0500

    Write tests for EnterName

--- src/EnterName.js
+++ src/EnterName.js
@@ -6,23 +6,30 @@ export default function EnterName() {
     const [input, setInput] = useState('')
     const [name, setName] = useContext(NameContext)
 
-    useEffect(() => {
+    //useEffect(() => () => setName(input), [])
         //might want to put this on the button's click
-        return function submitName() {
+        /*return function submitName() {
             setName(input)
         }
-    })
+    }, [input])*/
+
+    function handleOnSubmit(e) {
+        e.preventDefault()
+        if (input.length > 0) {
+            setName(input)
+        }
+    }
 
     return (
-        <div id="enter-name">
+        <form id="enter-name" onSubmit={(e) => handleOnSubmit(e)}>
             <h2>You won!</h2>
             <label htmlFor="name">Enter your name to submit your score.</label>
-            <input id="name" name="name" type="text" onChange={(e) => setInput(e.target.value)}></input>
+            <input id="name" name="name" type="text" required onChange={(e) => setInput(e.target.value)}></input>
             <button>
                 {input.length > 0 && <Link to="/leaderboard"/>}
                 Submit
             </button>
-        </div>
+        </form>
     )
 
 
--- src/EnterName.test.js
+++ src/EnterName.test.js
@@ -1,24 +1,54 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { MemoryRouter } from "react-router"
 import EnterName from "./EnterName"
 import NameContext from "./NameContext"
 describe('EnterName', () => {
 
-    //unit tests
+    let name = ''
+    //this will just return the inputted name
+    let setName = jest.fn()
+    
+    
+    //unit tests 
     it('renders with a heading, paragraph, and input', () => {
-        let name = ''
-        let setName = jest.fn()
         render(<NameContext.Provider value={[name, setName]}>
                 <EnterName />
             </NameContext.Provider>)
         expect(screen).toMatchSnapshot()
     })
 
-    it.skip('renders initially with no input value', () => {
-
+    it('renders initially with no input value', () => {
+        render(<NameContext.Provider value={[name, setName]}>
+            <EnterName />
+        </NameContext.Provider>)
+        expect(screen.getByRole("textbox").textContent.length).toEqual(0)
     })
 
-    it.skip('input state changes when input value changes', () => {
+    it('setName will fire when button is clicked', () => {
+        render(<NameContext.Provider value={[name, setName]}>
+            <EnterName />
+        </NameContext.Provider>, {wrapper: MemoryRouter})
+        userEvent.type(screen.getByRole("textbox"), "Jeremy")
+        userEvent.click(screen.getByRole("button"))
+        expect(setName.mock.calls).toHaveLength(1)
+    })
 
+    it('setName will be called with the current input state', () => {
+        render(<NameContext.Provider value={[name, setName]}>
+            <EnterName />
+        </NameContext.Provider>, {wrapper: MemoryRouter})
+        userEvent.type(screen.getByRole("textbox"), "Jeremy")
+        userEvent.click(screen.getByRole("button"))
+        expect(setName.mock.calls[0][0]).toEqual("Jeremy")
+    })
+
+    it('setName will not be called if the current input length is 0', () => {
+        render(<NameContext.Provider value={[name, setName]}>
+            <EnterName />
+        </NameContext.Provider>, {wrapper: MemoryRouter})
+        userEvent.click(screen.getByRole("button"))
+        expect(setName.mock.calls).toHaveLength(0)
     })
 
     //integration tests
