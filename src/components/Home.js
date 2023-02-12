2e0a1943ac0dfaaf5bc9e1c79fa730fab5a195e8
Joey Santia <joey.santia@gmail.com>
2023-02-12 15:55:21 -0500

    Add function that resets charArray when game restarts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,37 @@
+import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { CharArrayContext } from "./CharArrayContext";
 import CharList from "./CharList";
+import Patrick from "../img/patrick.png";
+import Benson from "../img/benson.webp";
+import Reptar from "../img/reptar.webp";
 import "./Home.css"
 
 export default function Home() {
+    const [charArray, setCharArray] = useContext(CharArrayContext)
+
+    useEffect(() => {
+        setCharArray([
+            {
+              name: "Reptar",
+              coords: "703,334,30",
+              img: { Reptar },
+              hasBeenFound: false,
+            },
+            {
+              name: "Benson",
+              coords: "542,166,25",
+              img: { Benson },
+              hasBeenFound: false,
+            },
+            {
+              name: "Patrick",
+              coords: "1155,219,25",
+              img: { Patrick },
+              hasBeenFound: false,
+            },
+          ])
+    }, [])
     return (
         <main id="home">
             <h2>Lost at PixelCon</h2>
