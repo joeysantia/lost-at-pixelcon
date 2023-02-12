e3a00f14e726245d270b9fe7cb196809bb669577
Joey Santia <joey.santia@gmail.com>
2023-02-12 13:02:21 -0500

    Add Link to submit button

--- src/components/EnterName.js
+++ src/components/EnterName.js
@@ -1,36 +1,38 @@
 import { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { NameContext } from "./NameContext"
+import './EnterName.css'
 
 export default function EnterName() {
     const [input, setInput] = useState('')
     const [name, setName] = useContext(NameContext)
 
-    //useEffect(() => () => setName(input), [])
-        //might want to put this on the button's click
-        /*return function submitName() {
+    useEffect(() => {
+        document.body.style = {
+            position: "fixed",
+            overflow: "hidden"
+        }
+
+        return () => {
+            console.log('entername cleanup')
             setName(input)
         }
-    }, [input])*/
+    }, [])
 
     function handleOnSubmit(e) {
         e.preventDefault()
-        console.log('i submitted', input.length)
-        console.log([name, setName])
         if (input.length > 0) {
             setName(input)
+            console.log('i submitted', input)
         }
     }
 
     return (
         <form id="enter-name" onSubmit={(e) => handleOnSubmit(e)}>
             <h2>You won!</h2>
-            <label htmlFor="name">Enter your name to submit your score.</label>
+            <label htmlFor="name">Enter your name to submit your time to the leaderboard.</label>
             <input id="name" name="name" type="text" required onChange={(e) => setInput(e.target.value)}></input>
-            <button>
-                {input.length > 0 && <Link to="/leaderboard"/>}
-                Submit
-            </button>
+            {input.length > 0 ? <Link to="../leaderboard"><button>Submit</button></Link> : <button>Submit</button>}
         </form>
     )
 
