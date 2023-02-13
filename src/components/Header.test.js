4aa69eae4fa0ea12a09f561bb040be505f0fa20a
Joey Santia <joey.santia@gmail.com>
2023-02-13 14:38:10 -0500

    Edit and delete tests

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,37 +1,39 @@
-import { render, screen } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
-import { MemoryRouter } from "react-router"
-import Header from "./Header"
-import { IsGameOverContext } from "./IsGameOverContext"
-import { TimeContext } from "./TimeContext"
-
-
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router";
+import { CharArrayContext } from "./CharArrayContext";
+import Header from "./Header";
+import { IsGameOverContext } from "./IsGameOverContext";
+import { TimeContext } from "./TimeContext";
 
 describe("Header", () => {
-    it('renders a navbar and a heading', () => {
-        render(<Header />, { wrapper: MemoryRouter})
-        expect(screen).toMatchSnapshot()
-    })
+  it("renders a navbar and a heading", () => {
+    render(<Header />, { wrapper: MemoryRouter });
+    expect(screen).toMatchSnapshot();
+  });
 
-    it('does not render a clock or "play again?" link when mode is welcomePage', () => {
-        render(<Header mode="welcomePage"/>, { wrapper: MemoryRouter})
-        expect(screen.queryByText("00 : 00: 00")).not.toBeInTheDocument()
-        expect(screen.queryByText("Play again?")).not.toBeInTheDocument()
-    })
+  it('does not render a clock or "play again?" link when mode is welcomePage', () => {
+    render(<Header mode="welcomePage" />, { wrapper: MemoryRouter });
+    expect(screen.queryByText("00 : 00: 00")).not.toBeInTheDocument();
+    expect(screen.queryByText("Play again?")).not.toBeInTheDocument();
+  });
 
-    it('renders a clock when the mode is level', () => {
-        render(
+  it("renders a clock and CharList when the mode is level", () => {
+    render(
+      <CharArrayContext.Provider value={[[], jest.fn()]}>
         <IsGameOverContext.Provider value={[false, jest.fn()]}>
-            <TimeContext.Provider value={[0, jest.fn()]}>
-                    <Header mode="level"/>
-                </TimeContext.Provider>
+          <TimeContext.Provider value={[0, jest.fn()]}>
+            <Header mode="level" />
+          </TimeContext.Provider>
         </IsGameOverContext.Provider>
-        , { wrapper: MemoryRouter})
-        expect(screen.getByText("00 : 00 : 00")).toBeInTheDocument()
-    })
+      </CharArrayContext.Provider>,
+      { wrapper: MemoryRouter }
+    );
+    expect(screen.getByText("00 : 00 : 00")).toBeInTheDocument();
+  });
 
-    it('renders a "Play Again?" button when mode is leaderboard', () => {
-        render(<Header mode="leaderboard"/>, { wrapper: MemoryRouter})
-        expect(screen.getByText("Play again?")).toBeInTheDocument()
-    })
-})
+  it('renders a "Play Again?" button when mode is leaderboard', () => {
+    render(<Header mode="leaderboard" />, { wrapper: MemoryRouter });
+    expect(screen.getByText("Play again?")).toBeInTheDocument();
+  });
+});
--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -1,24 +1,58 @@
-import { render, screen } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
-import { MemoryRouter } from "react-router"
-import Home from "./Home"
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { createMemoryHistory } from "history";
+import { MemoryRouter, Route, Router, Routes } from "react-router";
+import { CharArrayContext } from "./CharArrayContext";
+import Header from "./Header";
+import Home from "./Home";
+import { IsGameOverContext } from "./IsGameOverContext";
+import { NameContext } from "./NameContext";
+import { TimeContext } from "./TimeContext";
 
-jest.mock("./Header")
-jest.mock("./Level")
-jest.mock("./CharList")
-
+jest.mock("./Header");
+jest.mock("./Level");
+jest.mock("./CharList");
 
 describe("Home", () => {
-    //unit tests
-    it("renders with a heading, paragraph, and CharList", () => {
-        render(<Home />, { wrapper: MemoryRouter })
-        expect(screen).toMatchSnapshot()
-    })
+  let setCharArray = jest.fn();
+  let setName = jest.fn();
+  let setTime = jest.fn();
+  let setIsGameOver = jest.fn();
+  //unit tests
+  it("renders with a heading, paragraph, and CharList", () => {
+    render(
+      <CharArrayContext.Provider value={[[], setCharArray]}>
+        <IsGameOverContext.Provider value={[false, setIsGameOver]}>
+          <TimeContext.Provider value={[0, setTime]}>
+            <NameContext.Provider value={["", setName]}>
+              <Home />
+            </NameContext.Provider>
+          </TimeContext.Provider>
+        </IsGameOverContext.Provider>
+      </CharArrayContext.Provider>,
+      { wrapper: MemoryRouter }
+    );
+    expect(screen).toMatchSnapshot();
+  });
 
-    //integration tests
-    it("routes to the level when the button is clicked", () => {
-        render(<Home />, { wrapper: MemoryRouter })
-        userEvent.click(screen.getByRole("a"))
-        expect(screen.getByAltText("background")).toBeInTheDocument()
-    })
-})
+  it("resets context for charArray, time, and name", () => {
+    render(
+      <CharArrayContext.Provider
+        value={[[{ hasBeenFound: true }], setCharArray]}
+      >
+        <IsGameOverContext.Provider value={[true, setIsGameOver]}>
+          <TimeContext.Provider value={[10, setTime]}>
+            <NameContext.Provider value={["Jim", setName]}>
+              <Home />
+            </NameContext.Provider>
+          </TimeContext.Provider>
+        </IsGameOverContext.Provider>
+      </CharArrayContext.Provider>,
+      { wrapper: MemoryRouter }
+    );
+    expect(setCharArray.mock.calls).toHaveLength(1);
+    expect(setName.mock.calls).toHaveLength(1);
+    expect(setTime.mock.calls).toHaveLength(1);
+    expect(setIsGameOver.mock.calls).toHaveLength(1);
+  });
+});
